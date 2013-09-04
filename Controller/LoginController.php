08f5083d3f49871b1160ddec79faab8fd43a949a
nishant kumar <nishant_cbse@yahoo.com>
2013-09-05 02:37:30 +0530

    removing tmp folder

--- Controller/LoginController.php
+++ Controller/LoginController.php
@@ -3,13 +3,13 @@ class LoginController extends AppController {
 	public $name 		= 'login';
 	public $components  = array('RequestHandler','Email','Auth','Session','Cookie');
 	var $helpers        = array('Html', 'Form','Paginator','Js','JqueryEngine','Session');
-	var $uses         = array ('users');
+	//var $uses         = array ('users');
 function beforeFilter(){
 	$this->Auth->allow('login')
 	;}
 
 	public function login() {
-	$users = $this->users->find('all');
+	//$users = $this->users->find('all');
 		///debug($users);
 		$this->layout='login';
 	
